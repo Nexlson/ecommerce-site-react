a691d44ad746cfe1e57dc9af61d37a4a5917f29d
Nexlson <nelsonchoon112@gmail.com>
2021-07-15 23:07:26 +0800

    added header

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import HomePage from './pages/homepage/homepage-components';
 import { Switch, Route } from 'react-router-dom'
 import ShopPage from './pages/shop/shop.components';
+import Header from './components/header/header.components';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       {/* Exact -> determine to use exact url or not */}
       <Switch>
         <Route exact path='/' component={HomePage} />
