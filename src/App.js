cb798add307f29ae409a14a70ea52cea5bdcf3d9
Nexlson <nelsonchoon112@gmail.com>
2021-07-15 22:08:26 +0800

    updated router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage-components';
+import { Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      {/* Exact -> determine to use exact url or not */}
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+      </Switch>
     </div>
   );
 }
